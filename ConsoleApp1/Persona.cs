d94617e6ef0ad04d7b1b4c1f54e449b51cf503e7
ArielXT <arielzaratext@gmail.com>
2022-03-16 12:54:44 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Persona
+    {
+        //Metodos 
+        public String Nombres { get; set; }
+        public String Apellidos { get; set; }
+        public String NombresCompletos { get; set; }
+        public int Edad { get; set; }
+
+        //Atributos
+        public string EsMayorEdad(int edadMinima)
+        {
+            String mensaje = "Es Menor de Edad";
+            if (Edad>18){
+                mensaje = "Es Mayor de Edad";
+            }
+            return mensaje;
+        }
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = string.Concat(Nombres, "", Apellidos);
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de Variables
+            int edad = 20;
+            string nombre = "Ariel";
+            float sueldo = 8500;
+            DateTime hoy = DateTime.Now;
+
+            //Console.WriteLine(edad);
+            //Console.WriteLine(nombre);
+            //Console.WriteLine(sueldo);
+            //Console.WriteLine(hoy);
+
+            //Declaracion de objetos
+
+            Persona persona = new Persona();
+            persona.Nombres = "Ariel";
+            persona.Apellidos = " Zarate";
+            persona.Edad = 20;
+
+            Persona persona1 = new Persona
+            {
+                Nombres = "Ariana",
+                Apellidos = " Avila",
+                Edad = 8,
+            };
+
+            List<Persona> personas = new List<Persona>();
+            personas.Add(persona);
+            personas.Add(persona1);
+            foreach (var item in personas)
+            {
+                string mensaje = item.EsMayorEdad(18);
+                item.AsignarNombresCompletos();
+                Console.WriteLine(mensaje);
+                Console.WriteLine(item.NombresCompletos);
+            }
+
+            Console.Read();
+        }
+    }
+}
